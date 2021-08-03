0d0de0f3788824cd4b27be750ae2f45deb00328d
Sulayman <muhammadsulaymannkhan@gmail.com>
2021-08-04 04:22:26 +0500

    Private route basic implementation

--- src/Components/Routes/PrivateRoute.jsx
+++ src/Components/Routes/PrivateRoute.jsx
@@ -4,28 +4,15 @@ import { useState,useEffect } from 'react';
 import { useGetUser } from '../apis/useGetUser';
 
  export const PrivateRoute = ({component: Component, ...rest}) => {
-     const [isLogin,setislogin]= useState()
-     const [res,getUser]=useGetUser()
-
-     useEffect(() => {
-        getUser()
-      }, [])
-    
-      useEffect(() => {
-        if (res.status===200) {
-          setislogin(true)
-        }else{
-            setislogin(false)
-        }
-    
-      }, [res])
+     const token=localStorage.getItem("token")
+     
 
     return (
 
         // Show the component only when the user is logged in
         // Otherwise, redirect the user to /signin page
         <Route {...rest} render={props => (
-            isLogin ?
+            token?
                 <Component {...props} />
             : <Redirect to="/login" />
         )} />
